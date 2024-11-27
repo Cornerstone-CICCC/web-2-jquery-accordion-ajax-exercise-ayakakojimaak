234de251d7f8dfe4e30d80bc9b4f844451c95b38
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-11-27 11:50:23 -0800

    add jQuery

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-$(function() {
-  // your code here
-})
+$(function () {
+  $(".accordion-header").on("click", function () {
+    $(".accordion-header").next("div").slideUp();
+    if ($(this).next("div").css("display") === "none") {
+      $(this).next("div").slideDown();
+    }
+  });
+
+  $(".todos > button").on("click", function (event) {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      dataType: "json",
+    })
+      .done(function (data) {
+        const data_stringify = JSON.stringify(data.todos);
+        const data_json = JSON.parse(data_stringify);
+        let html = "";
+        data_json.forEach((list) => {
+          html += `<li class="todo-${list.id} ${list.completed ? "completed" : ""}">${list.todo}</li>`;
+        });
+        $(".todos > ul").append(html);
+      })
+      .fail(function () {
+        alert("error");
+      });
+  });
+});
